124201fe15a8c5268d9074a6b6d124740caaad92
revol.cai <revol.cai@daocloud.io>
2018-03-07 15:15:50 +0800

    deprecate python3.4 support since aiohttp require python>=3.5.3

--- setup.py
+++ setup.py
@@ -60,7 +60,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
