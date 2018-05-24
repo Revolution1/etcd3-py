45d2489ed526fa9f2dc63ef005b5f24f82a980b2
revol.cai <revol.cai@daocloud.io>
2018-05-24 19:12:46 +0800

    fix pip.req

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@
 import os
 import platform
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 PY2 = platform.python_version_tuple()[0] == '2'
