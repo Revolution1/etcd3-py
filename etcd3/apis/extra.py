fbafa4e44839362ec9273a4bbd936b1b243b28f5
Ales Bublik <ales.bublik@ixperta.com>
2018-11-05 15:10:10 +0100

    call extra api with headers included

--- etcd3/apis/extra.py
+++ etcd3/apis/extra.py
@@ -14,7 +14,7 @@ class ExtraAPI(BaseAPI):
 
         :return: EtcdVersion
         """
-        resp = self._get(self._url('/version'))
+        resp = self._get(self._url('/version'), headers=self.headers)
         self._raise_for_status(resp)
         return EtcdVersion(**resp.json())
 
@@ -24,7 +24,7 @@ class ExtraAPI(BaseAPI):
 
         :return: EtcdVersion
         """
-        resp = self._get(self._url('/health'))
+        resp = self._get(self._url('/health'), headers=self.headers)
         self._raise_for_status(resp)
         return resp.json()['health']
 
@@ -34,7 +34,7 @@ class ExtraAPI(BaseAPI):
 
         :return: str
         """
-        resp = self._get(self._url('/metrics'))
+        resp = self._get(self._url('/metrics'), headers=self.headers)
         self._raise_for_status(resp)
         metrics = resp.content
         if not isinstance(metrics, six.text_type):
