5351aa20b4e59c6101aa5f9ca6c19230302bbe87
Renjie Cai <revol.cai@gmail.com>
2019-07-03 12:52:54 +0800

    fix release unlocked lock error in keepalive daemon thread (#107)
    * fix release unlocked lock error in keepalive daemon thread
    
    * fix test

--- tests/test_lease_util.py
+++ tests/test_lease_util.py
@@ -3,6 +3,7 @@ import time
 
 import mock
 import pytest
+import six
 
 from etcd3.client import Client
 from tests.docker_cli import docker_run_etcd_main
@@ -73,3 +74,51 @@ def test_lease_keep(client):
     lease.grant()
     assert lease.alive()
     lease.revoke()
+
+
+def test_release_unlocked_lock(client):
+    import threading
+
+    condition = threading.Condition
+    if six.PY2:  # pragma: no cover
+        condition = threading._Condition
+
+    class Lock(condition):  # pragma: no cover
+        def acquire(self, blocking=True, timeout=None):
+            print("acquiring lock")
+            if six.PY3:
+                return super(Lock, self).acquire(blocking, timeout)
+            else:
+                return super(Lock, self).acquire(blocking)
+
+        def release(self):
+            print("releasing lock")
+            return super(Lock, self).release()
+
+        def notify_all(self):
+            print('notify all')
+            return super(Lock, self).notify_all()
+
+        def wait(self, timeout=None):
+            print('waiting lock')
+            return super(Lock, self).wait(timeout)
+
+        def __enter__(self):
+            print("entering lock")
+            return super(Lock, self).__enter__()
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            print("exiting lock")
+            return super(Lock, self).__exit__(exc_type, exc_val, exc_tb)
+
+    L = client.Lease(ttl=5)
+    L._lock = Lock()
+    print("start with lock")
+    with L as lease:
+        client.put('foo', 'bar', lease=lease.ID)
+        client.put('fizz', 'buzz', lease=lease.ID)
+        r = lease.time_to_live(keys=True)
+        assert set(r.keys) == {b'foo', b'fizz'}
+        assert lease.alive()
+        print("end with lock")
+    assert L._keepalive_error is None
